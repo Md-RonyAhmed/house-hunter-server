154a36a41fa0c6ed93b50639616419d4e02a90c6
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-07-17 17:58:05 +0600

    [DONE] Updated

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import httpStatus from 'http-status';
+import cookieParser from 'cookie-parser';
+
+//create app
+const app: Application = express();
+
+// use cors
+app.use(cors());
+
+// json parser
+app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
+
+// url parser
+app.use(express.urlencoded({ extended: true }));
+
+//application route
+// app.use('/api/v1', routes);
+
+//home route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to our application');
+});
+
+//global error handler
+// app.use(globalErrorHandler);
+
+//handle not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'Api Not Found',
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
